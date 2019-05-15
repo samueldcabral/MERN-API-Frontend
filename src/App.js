ea3cb45ac4f4503f744de6102686c046936dbd60
samueldcabral <samueldcabral@gmail.com>
2019-05-15 02:56:31 -0300

    still on git config

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React and Have Fun
+          Learn React and Have Fun!
         </a>
       </header>
     </div>
